95633a44006f62c4d2d4cbc54b1e4045c3ae885b
rajon-git <rajon.zhsust15@gmail.com>
2024-10-05 10:01:50 +0600

    post method created with movie name in imdb_list

--- imdb_list/api/serializers.py
+++ imdb_list/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
+from imdb_list.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self,validated_data):
+        return Movie.objects.create(**validated_data)
--- imdb_list/api/views.py
+++ imdb_list/api/views.py
@@ -6,11 +6,19 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
-@api_view()
+@api_view(['GET','POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data = request.data)  #data received from user, user send data
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
 
 @api_view()
 def movie_details(request,id):
