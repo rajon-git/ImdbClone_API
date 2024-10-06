627477e6737d4e8587edb211baf0d8170f17ec04
rajon-git <rajon.zhsust15@gmail.com>
2024-10-06 18:57:13 +0600

    class based urls implement

--- imdb_list/api/urls.py
+++ imdb_list/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import movie_list, movie_details
+# from .views import movie_list, movie_details
+from .views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path('list/', movie_list, name="movie_list"),
-    path('<int:id>/', movie_details, name="movie_details"),
+    path('list/', MovieListAV.as_view(), name="movie_list"),
+    path('<int:id>/', MovieDetailAV.as_view(), name="movie_details"),
 ]
