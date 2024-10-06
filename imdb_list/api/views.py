43e8fa77c17c3c683982466a3e0a086752b1a7f4
rajon-git <rajon.zhsust15@gmail.com>
2024-10-06 18:45:37 +0600

    class based post view implement

--- imdb_list/api/views.py
+++ imdb_list/api/views.py
@@ -13,6 +13,14 @@ class MovieListAV(APIView):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
+
+    def post(self,request):
+        serializer = MovieSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, ststus=400)
 # function based view
 # @api_view(['GET','POST'])
 # def movie_list(request):
