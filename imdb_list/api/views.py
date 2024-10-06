8bc729fe363f71c687fef6222a8bb6c150c4b6f8
rajon-git <rajon.zhsust15@gmail.com>
2024-10-06 18:42:10 +0600

    class based views get request create

--- imdb_list/api/views.py
+++ imdb_list/api/views.py
@@ -5,48 +5,55 @@ from .serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
-@api_view(['GET','POST'])
-def movie_list(request):
-    if request.method == 'GET':
+# class based view
+class MovieListAV(APIView):
+    def get(self,request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = MovieSerializer(data = request.data)  #data received from user, user send data
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        else:
-            return Response(serializer.errors, status=400)
+# function based view
+# @api_view(['GET','POST'])
+# def movie_list(request):
+#     if request.method == 'GET':
+#         movies = Movie.objects.all()
+#         serializer = MovieSerializer(movies, many=True)
+#         return Response(serializer.data)
+#     if request.method == 'POST':
+#         serializer = MovieSerializer(data = request.data)  #data received from user, user send data
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         else:
+#             return Response(serializer.errors, status=400)
 
-@api_view(['GET','PUT','DELETE'])
-def movie_details(request,id):
-    if request.method == 'GET':
-        try:
-            movie = Movie.objects.get(pk=id)
-        except Movie.DoesNotExist:
-            return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)
+# @api_view(['GET','PUT','DELETE'])
+# def movie_details(request,id):
+#     if request.method == 'GET':
+#         try:
+#             movie = Movie.objects.get(pk=id)
+#         except Movie.DoesNotExist:
+#             return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+#         serializer = MovieSerializer(movie)
+#         return Response(serializer.data)
     
-    if request.method == 'PUT':
-        try:
-            movie = Movie.objects.get(pk=id)
-        except Movie.DoesNotExist:
-            return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = MovieSerializer(movie,data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=400)
+#     if request.method == 'PUT':
+#         try:
+#             movie = Movie.objects.get(pk=id)
+#         except Movie.DoesNotExist:
+#             return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+#         serializer = MovieSerializer(movie,data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status=400)
         
-    if request.method == 'DELETE':
-        try:
-            movie = Movie.objects.get(pk=id)
-        except Movie.DoesNotExist:
-            return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
-        movie.delete()
-        return Response({'message': f'{movie.name} removed successfully'}, status=status.HTTP_204_NO_CONTENT)
+#     if request.method == 'DELETE':
+#         try:
+#             movie = Movie.objects.get(pk=id)
+#         except Movie.DoesNotExist:
+#             return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+#         movie.delete()
+#         return Response({'message': f'{movie.name} removed successfully'}, status=status.HTTP_204_NO_CONTENT)
