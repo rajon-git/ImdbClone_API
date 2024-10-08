b8a43f951e05b9c36f1960e8c108331e38665be4
rajon-git <rajon.zhsust15@gmail.com>
2024-10-08 10:09:25 +0600

    model serializer created

--- imdb_list/api/serializers.py
+++ imdb_list/api/serializers.py
@@ -1,25 +1,44 @@
 from rest_framework import serializers
 from imdb_list.models import Movie
 
-def name_length(value):
-    if len(value) < 2:
-        raise serializers.ValidationError('Name should be at least 2 characters long.')
-    
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"  #if i want all fields to show then write this 
+        # fields = ['id','name','description']  #if i want to show some selected fields 
+        # exclude = ['name']  #if i want dont show selected fields 
+        
+    def validate_name(self,value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Name should be at least 2 characters long.')
+        else:
+            return value
+        
+    def validate(self,data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Name and description should not be the same.')
+        else:
+            return data
 
-    def create(self,validated_data):
-        return Movie.objects.create(**validated_data)
+# def name_length(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError('Name should be at least 2 characters long.')
     
-    def update(self,instance,validated_data):  #here instance carried old or current values which i want updated
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     def create(self,validated_data):
+#         return Movie.objects.create(**validated_data)
+    
+#     def update(self,instance,validated_data):  #here instance carried old or current values which i want updated
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
     
     # field label validation
     # def validate_name(self,value):
