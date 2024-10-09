f07ff517bc03d00293d91149ec8a99e78d024ab1
rajon-git <rajon.zhsust15@gmail.com>
2024-10-09 22:07:15 +0600

    create streamlist model

--- imdb_list/models.py
+++ imdb_list/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
 class WatchList(models.Model):
     name = models.CharField(max_length=100)
     storyline = models.CharField(max_length=500)
