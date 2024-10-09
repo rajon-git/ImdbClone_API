ce2b0cb908a0d141b2c6ef731d1cd288a50683af
rajon-git <rajon.zhsust15@gmail.com>
2024-10-09 22:25:08 +0600

    updated url

--- imdb_list/api/urls.py
+++ imdb_list/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from .views import movie_list, movie_details
-from .views import MovieListAV, MovieDetailAV
+from .views import WatchListAV, WatchListDetailAV, StreamPlatformAV
 
 urlpatterns = [
-    path('list/', MovieListAV.as_view(), name="movie_list"),
-    path('<int:id>/', MovieDetailAV.as_view(), name="movie_details"),
+    path('list/', WatchListAV.as_view(), name="movie_list"),
+    path('<int:id>/', WatchListDetailAV.as_view(), name="movie_details"),
+    path('stream/', StreamPlatformAV.as_view(), name="stream"),
 ]
