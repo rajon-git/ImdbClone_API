d6c5d78435900a0f45f70e0460ebc48cc51945b1
rajon-git <rajon.zhsust15@gmail.com>
2024-10-11 20:10:35 +0600

    django model relationship implement

--- imdb_clone/urls.py
+++ imdb_clone/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('imdb_list.api.urls')),
+    path('watch/', include('imdb_list.api.urls')),
 ]
