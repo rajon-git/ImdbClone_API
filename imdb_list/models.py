034a5dc07124dba8bca764952a2e98113daa5678
rajon-git <rajon.zhsust15@gmail.com>
2024-10-14 15:57:29 +0600

    review model created

--- imdb_list/models.py
+++ imdb_list/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class StreamPlatform(models.Model):
@@ -18,3 +19,14 @@ class WatchList(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Review(models.Model):
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    description = models.CharField(max_length=500, null=True)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='reviews')
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.rating)+" | "+self.watchlist.name
