78de2e3cfdabbc028d67fc2e811d2a36b9bdc478
rajon-git <rajon.zhsust15@gmail.com>
2024-10-14 21:22:48 +0600

    review details and single product all reviews implemented

--- imdb_list/api/urls.py
+++ imdb_list/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 # from .views import movie_list, movie_details
-from .views import WatchListAV, WatchListDetailAV, StreamPlatformAV, ReviewList, ReviewDetail
+from .views import WatchListAV, WatchListDetailAV, StreamPlatformAV, ReviewList, ReviewDetail, ReviewCreate
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name="movie_list"),
     path('<int:id>/', WatchListDetailAV.as_view(), name="movie_details"),
     path('stream/', StreamPlatformAV.as_view(), name="stream"),
 
-    path('review/', ReviewList.as_view(), name="review-list"),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
+    # path('review/', ReviewList.as_view(), name="review-list"),
+    # path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
+
+    path('stream/<int:pk>/review-create/', ReviewCreate.as_view(), name="review-create"),
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name="review-list"),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
 ]
