15f7937be0bee5a57903827caeb111456270f500
rajon-git <rajon.zhsust15@gmail.com>
2024-10-14 17:47:02 +0600

    review details created with mixins generics view

--- imdb_list/api/urls.py
+++ imdb_list/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # from .views import movie_list, movie_details
-from .views import WatchListAV, WatchListDetailAV, StreamPlatformAV, ReviewList
+from .views import WatchListAV, WatchListDetailAV, StreamPlatformAV, ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name="movie_list"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name="stream"),
 
     path('review/', ReviewList.as_view(), name="review-list"),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
 ]
--- imdb_list/api/views.py
+++ imdb_list/api/views.py
@@ -8,7 +8,15 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics, mixins
 
-# class based view
+# class based view with generic methods
+
+class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Review.objects.all()
